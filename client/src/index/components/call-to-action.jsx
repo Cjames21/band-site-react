2fbfef51be26e33ae711f23a56e680b8dc646d49
Charles.James <charlestdjames1@gmail.com>
2018-03-28 15:30:23 -0400

    Added a json-models file
    Added json-models file to act as this application's data center for all
    templates in the application that are reuseable.
    
    This will enable quick and easy customization among a large number of
    different potential clients.

--- client/src/index/components/call-to-action.jsx
+++ client/src/index/components/call-to-action.jsx
@@ -1,1 +1,29 @@
 import React from 'react';
+
+
+export default class CallToAction extends React.Component {
+
+  constructor() {
+    super();
+
+    this.callToAction = this.callToAction.bind(this);
+  }
+
+  callToAction() {
+    console.log("Shit just went down!");
+  }
+
+
+  render() {
+    return (
+      <div className="call-to-action">
+        <button
+          type="button"
+          className="call-to-action-button"
+          onClick={this.callToAction}>
+          AVAILABLE NOW!
+        </button>
+      </div>
+    );
+  }
+}
--- client/src/index/components/header-centerfold.jsx
+++ client/src/index/components/header-centerfold.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import CallToAction from './call-to-action.jsx';
 
 
 export default class HeaderCenterfold extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    // Should receive an object containing a mainText, and a subText string that
+    // will be used to render this component dynamically
+    const centerfoldInfo = {this.props.centerfoldData};
+  }
 
   render() {
     return (
       <div className="header-centerfold">
-
+        <h1 className=""></h1>
+        <h3 className=""></h3>
+        <CallToAction />
       </div>
     );
   }
